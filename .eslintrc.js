2cfd2d7a9f2113d03fd9dc4e3e866d67d8dc4335
Victor Batalha <madivcb@gmail.com>
2019-08-25 20:38:39 -0300

    feat: disabled props spreading rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'airbnb', 'prettier', 'prettier/react'
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -17,13 +15,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-    'prettier',
-    'react-hooks',
-    'jsx-a11y',
-    'import',
-  ],
+  plugins: ['react', 'prettier', 'react-hooks', 'jsx-a11y', 'import'],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
@@ -33,19 +25,20 @@ module.exports = {
       },
     ],
     'import/prefer-default-export': 'off',
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_'}],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'global-require': 'off',
     'no-param-reassign': 'off',
     camelcase: 'off',
     'no-console': ['error', { allow: ['tron'] }],
     'react-hooks/rules-of-hooks': 'off',
     'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-props-no-spreading': 'off',
   },
   settings: {
     'import/resolver': {
       'babel-plugin-root-import': {
         rootPathSuffix: 'src',
-      }
-    }
-  }
+      },
+    },
+  },
 };
