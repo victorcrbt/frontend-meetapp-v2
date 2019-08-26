ff71aa9114b91f0d442f6e2301115e35ea6b1d4f
Victor Batalha <madivcb@gmail.com>
2019-08-26 17:27:29 -0300

    feat: set authorization token for requests

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -13,6 +13,8 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
 
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
     yield put(signInSuccess(token, user));
     history.push('/dashboard');
   } catch (err) {
@@ -36,8 +38,19 @@ export function signOut() {
   history.push('/');
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
   takeLatest('@auth/SIGN_OUT', signOut),
+  takeLatest('persist/REHYDRATE', setToken),
 ]);
