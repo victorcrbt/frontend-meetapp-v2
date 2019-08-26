a4c2a39b451c33904dae5ea5a444a82428156a03
Victor Batalha <madivcb@gmail.com>
2019-08-26 17:07:04 -0300

    feat: meetup page stylization

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdAdd, MdRemoveRedEye } from 'react-icons/md';
 
-// import { Container } from './styles';
+import {
+  Container,
+  Header,
+  PageTitle,
+  NewMeetupButton,
+  Meetups,
+  Meetup,
+  MeetupTitle,
+  MeetupDate,
+  Pagination,
+  PageButton,
+} from './styles';
+
+const data = [1, 2, 3, 4, 5];
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <Header>
+        <PageTitle>Meus meetups</PageTitle>
+
+        <NewMeetupButton type="button">
+          <MdAdd size={24} color="#fff" />
+          Nova meetup
+        </NewMeetupButton>
+      </Header>
+
+      <Meetups>
+        {data.map(meetup => (
+          <Meetup>
+            <MeetupTitle>Meetup de ReactJS</MeetupTitle>
+
+            <div>
+              <MeetupDate>24 de Junho, às 20h</MeetupDate>
+              <Link to={`/meetup/${meetup}`}>
+                <MdRemoveRedEye size={20} color='#eee' />
+              </Link>
+            </div>
+          </Meetup>
+        ))}
+      </Meetups>
+
+      <Pagination>
+        <PageButton>Anterior</PageButton>
+
+        <PageButton>Próxima</PageButton>
+      </Pagination>
+    </Container>
+  );
 }
