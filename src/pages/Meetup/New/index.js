55224bbeb3755dec001c05bcb51fa98912893184
Victor Batalha <madivcb@gmail.com>
2019-08-27 00:04:46 -0300

    feat: new meetup page stylization

--- src/pages/Meetup/New/index.js
+++ src/pages/Meetup/New/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import BannerInput from '~/components/BannerInput';
+import TextInput from '~/components/TextInput';
+import TextArea from '~/components/TextArea';
+import DateInput from '~/components/DateInput';
+import SubmitButton from '~/components/SubmitButton';
+
+import { Container, PageTitle, MeetupForm } from './styles';
 
 export default function NewMeetup() {
-  return <h1>New</h1>;
+  function handleSubmit(data) {
+    console.log(data);
+  }
+
+  return (
+    <Container>
+      <PageTitle>Novo meetup</PageTitle>
+
+      <MeetupForm onSubmit={handleSubmit}>
+        <BannerInput name="banner_id" />
+
+        <TextInput name="title" placeholder="Digite o título do seu meetup" />
+        <TextArea
+          name="description"
+          placeholder="Descreva o seu meetup em algumas linhas"
+        />
+        <DateInput name="date" placeholder="Digite a data do seu meetup" />
+        <TextInput
+          name="location"
+          placeholder="Digite o local em que ocorerrá o meetup"
+        />
+
+        <SubmitButton type="submit">Cadastrar</SubmitButton>
+      </MeetupForm>
+    </Container>
+  );
 }
---
+++ src/pages/Meetup/New/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Form } from '@rocketseat/unform';
+
+export const Container = styled.div`
+  margin-top: 25px;
+`;
+
+export const PageTitle = styled.h1`
+  margin-bottom: 20px;
+  color: #fff;
+  font-size: 42px;
+`;
+
+export const MeetupForm = styled(Form)``;
