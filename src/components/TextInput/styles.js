1ddb87ee315a43718f50b40c096facf599dc1198
Victor Batalha <madivcb@gmail.com>
2019-08-27 11:06:09 -0300

    feat: validation message stylization

--- src/components/TextInput/styles.js
+++ src/components/TextInput/styles.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  margin-bottom: 10px;
 
   input {
     width: 100%;
@@ -9,12 +10,16 @@ export const Container = styled.div`
     border-radius: 5px;
     border: 0;
     padding: 10px;
-    margin-bottom: 10px;
     background: rgba(0, 0, 0, 0.2);
     color: rgba(255, 255, 255, 0.5);
 
     &::placeholder {
       color: rgba(255, 255, 255, 0.5);
     }
+  }
+
+  span {
+    color: #EB3750;
+    font-weight: bold;
   }
 `;
